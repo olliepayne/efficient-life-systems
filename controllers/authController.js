b2eb11770a2668f7c81e3ce72012b66dac245549
olliepayne <olliepayne@me.com>
2021-02-09 17:25:01 -0700

    begin implementing signup

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,16 @@
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+
 module.exports = {
  signup
 }
 
 function signup(req, res) {
- 
+ // destruct req object
+ // check for required fields
+ // check unique user
+ // create user
+ // hash and save password
+ // sign a web token, and send back in the response object
 }
